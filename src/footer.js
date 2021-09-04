be0613a69accafe9c69ddb447e7b9aa5b70642cc
suraj more <29055479+surajmore98@users.noreply.github.com>
2021-09-04 17:03:55 +0530

    file structure changed.

---
+++ src/footer.js
@@ -0,0 +1,25 @@
+import './App.css';
+import github from './assets/github.svg';
+import twitter from './assets/twitter.svg';
+import linkedin from './assets/linkedin.svg';
+
+function Footer() { 
+    return (
+        <footer class="footer">
+            <div>social media presence</div>
+            <ul class="list-non-bullet">
+                <li class="list-item-inline">
+                    <a href="https://github.com/surajmore98" class="link"><img alt="github" src={github} title="github"/></a>
+                </li>
+                <li class="list-item-inline">
+                    <a href="https://www.linkedin.com/in/suraj-more-315b43119" class="link"><img src={linkedin} title="linkedin"/></a>
+                </li>
+                <li class="list-item-inline">
+                    <a href="/" class="link"><img src={twitter} title="twitter"/></a>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
